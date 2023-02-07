59a7ffa70535a15770f7db40412c83d638c8ff3b
kapil makhija <kapilm3828@gmail.com>
2023-02-08 01:29:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const sechand = document.querySelector(".sec-hand");
+const minhand = document.querySelector(".min-hand");
+const hourhand = document.querySelector(".hour-hand");
+const clock = document.querySelector(".clock");
+function settime(){
+const now = new Date();
+const seconds = now.getSeconds();
+const secindeg = ((seconds/60)*360) + 90;
+sechand.style.transform = `rotate(${secindeg}deg)`;
+
+const minutes = now.getMinutes();
+const minindeg = ((minutes/60)*360) + 90;
+minhand.style.transform = `rotate(${minindeg}deg)`;
+
+const hour = now.getHours();
+const hourindeg = ((hour/60)*360) + 90;
+hourhand.style.transform = `rotate(${hourindeg}deg)`;
+
+
+clock.style.borderColor = `rgba(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256} , 0.5)`;
+
+
+}
+
+setInterval(settime , 1000);
